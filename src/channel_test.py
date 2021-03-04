bd674461a482db6d1dce8330d9b02dad966653f9
Thomas Chen <z5313390@ad.unsw.edu.au>
2021-03-04 19:47:04 +1100

    Ideas for tests for channel

---
+++ src/channel_test.py
@@ -0,0 +1,25 @@
+# Tests for Channel Functions
+# TC - channel_messages
+
+
+def test_invalidChannel_messages():
+    #Test 1: Invalid channel
+    user = auth_register_v1()
+    
+    # make user connect to a channel that has not been created
+
+
+
+def test_startGreaterEnd_messages():
+    #Test 2: Start is greater than the total messages
+
+
+
+def test_userInvalid_messages():
+    #Test 3: User is invalid to check messages
+
+    user1 = auth_register_v1()
+
+    #make user1 create a channel then user2 try see the messages of their channel
+
+
