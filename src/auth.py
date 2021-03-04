34d19dcedd734ae78ff215eda598bf5eec800add
Rodney <z5338429@unsw.edu.au>
2021-03-04 21:25:25 +1100

    Completed auth_login_v1 function of auth.py | Function passes auth_login_test.py and works as intended | Fixed a bug in auth_register_v1

--- src/auth.py
+++ src/auth.py
@@ -3,9 +3,29 @@ from src.database import memberSize, accData
 from src.error import InputError
 
 def auth_login_v1(email, password):
-    return {
-        'auth_user_id': 1,
-    }
+
+    # Check if email is valid
+    isValidEmail = bool(re.match("^[a-zA-Z0-9]+[\\._]?[a-zA-Z0-9]+[@]\\w+[.]\\w{2,3}$", email))
+    if isValidEmail:
+        # Email is valid
+        # Check if user exists for that email
+        if search_email(email):
+            # User exists, now check if password is correct
+            if search_password(password):
+                # Password exists, return user_id
+                return {
+                    'auth_user_id': get_user_id(email),
+                }
+            else:
+                # Password not found
+                raise InputError("Wrong password")
+        else:
+            # User does not exist
+            raise InputError("A registered user does not exist with given email")
+    else:
+        # Email is not valid
+        raise InputError("Email given is not a valid email")
+
 
 def auth_register_v1(email, password, name_first, name_last):
 
@@ -50,13 +70,14 @@ def auth_register_v1(email, password, name_first, name_last):
                 "handle": userHandle, 
             }
             accData.append(userData)
+            userID = memberSize
             memberSize += 1
     else:
         # Error
         raise InputError("Error: Email is not valid")
 
     return {
-        'auth_user_id': memberSize,
+        'auth_user_id': userID,
     }
 
 def search_email(email):
@@ -65,11 +86,23 @@ def search_email(email):
             return True
     return False
 
+def search_password(password):
+    for items in accData:
+        if items["password"] == password:
+            return True
+    return False
+
 def search_handle(currUserHandle):
     for items in accData:
         if items["handle"] == currUserHandle:
             return True
     return False
+
+def get_user_id(email):
+    for items in accData:
+        if items["email"] == email:
+            userID = items["id"]
+            return userID
 
 def append_handle(currUserHandle):
     # Check number of users with same handle
