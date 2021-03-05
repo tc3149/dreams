72cc18f03af090152fc7155eaa20330f069340c7
Thomas Chen <z5313390@ad.unsw.edu.au>
2021-03-05 16:53:01 +1100

    Finished clear function

--- src/other.py
+++ src/other.py
@@ -1,5 +1,19 @@
+import pytest
+from src.auth import auth_register_v1, auth_login_v1
+from src.error import InputError, AccessError
+from src.channel import channel_messages_v1
+from src.channels import channels_create_v1
+from src.database import memberSize, accData, channelList
+
 def clear_v1():
-    pass
+    '''
+    Reset Everything to default state
+    '''
+    global accData, channelList, memberSize
+    accData.clear() 
+    channelList.clear() 
+    memberSize = 0
+
 
 def search_v1(auth_user_id, query_str):
     return {
