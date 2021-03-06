2b5e1cead813b7c6df8da17b6f3632ff060f9e92
Colin Ngo <z5310745@ad.unsw.edu.au>
2021-03-07 00:44:53 +1100

    added test cases for uhhhh joining function

--- tests/channel_test.py
+++ tests/channel_test.py
@@ -5,49 +5,7 @@ from src.error import InputError, AccessError
 from src.channel import channel_messages_v1
 from src.channels import channels_create_v1
 from src.database import accData, channelList
-import channels.py
-
-# Channel 
-
-def test_no_channels():
-    clear_v1():
-    user1 = auth_register_v1("gordonl1@gmail.com", "gordonisverygood", "Gordon", "Liang")
-    assert channels_list_v1(user1.get("auth_user_id")) == []
-
-def test_channels_valid():
-    clear_v1():
-    user1 = auth_register_v1("visejoy@gmail.com", "sikk", "Jonathan", "Qiu")
-
-    newchannel1 = channels_create_v1(user1.get("auth_user_id"), "channel1", True)
-    newchannel2 = channels_create_v1(user1.get("auth_user_id"), "channel2", True)
-    newchannel3 = channels_create_v1(user1.get("auth_user_id"), "channel3", True)
-
-    listchannel = channels_list_v1(user1.get("auth_user_id"))
-
-    assert listchannel.get('channels')[0]["channel_id"] == channel1.get("channel_id")
-    assert listchannel.get('channels')[1]["channel_id"] == channel2.get("channel_id")
-    assert listchannel.get('channels')[2]["channel_id"] == channel3.get("channel_id")
-    assert len(channel_list.get('channels')) == 3
-
-
-def test_both_priv_pub():
-    clear_v1():
-    user1 = auth_register_v1("visejoy@gmail.com", "sikk", "Jonathan", "Qiu") 
-    user2 = auth_register_v1("thomaschen@gmail.com", "rak", "Thomas", "Chen")
-
-    newchannel1 = channels_create_v1(user1.get("auth_user_id"), "channel1", True)
-    newchannel2 = channels_create_v1(user1.get("auth_user_id"), "channel2", False)
-    newchannel3 = channels_create_v1(user1.get("auth_user_id"), "channel3", True)
-
-    user1_id = user1.get("auth_user_id")
-
-    listchannel = channels_list_v1(user1_id.get('channels'))
-
-    assert listchannel.get('channels')[0]["channel_id"] == channel1.get("channel_id")
-    assert listchannel.get('channels')[1]["channel_id"] == channel2.get("channel_id")
-    assert listchannel.get('channels')[2]["channel_id"] == channel3.get("channel_id")
-    assert len(channel_list.get('channels')) == 3
-
+from src.channel import channel_join_v1
 
 
 # Channel Messages Tests
@@ -104,4 +62,72 @@ def test_channel_messages_endnegativeone():
     channel = channels_create_v1(user["auth_user_id"], "testchannel", True)
     messages = channel_messages_v1(user["auth_user_id"], channel["channel_id"], 0)
     assert messages["end"] == -1
+
+
+
+# TEST JOINING
+
+# VALID CASES
+
+def test_join_correct():
+    clear_v1()
+    user1 = auth_register_v1("visejoy@gmail.com", "sikk423423432", "Jonathan", "Qiu")
+    user2 = auth_register_v1("thomaschen333@gmail.com", "rak4234324243", "Thomas", "Chen")
+    user3 = auth_register_v1("gordonl1@gmail.com", "gordonisreallygood12", "Gordon", "Liang")
+    channel = channels_create_v1(user1["auth_user_id"], "testchannel", True)
+
+    inner = channelList[0]
+    channel_join_v1(user2["auth_user_id"], channel["channel_id"])
+    channel_join_v1(user3["auth_user_id"], channel["channel_id"])
+
+    assert inner["member_ids"] == [0,1,2]
+
+    
+# FAIL CASES
+
+# joining empty
+def test_joining_nonexistant_channel():
+    clear_v1()
+    user1 = auth_register_v1("visejoy@gmail.com", "sikk42342432", "Jonathan", "Qiu")
+    user2 = auth_register_v1("thomaschen@gmail.com", "rak432432424", "Thomas", "Chen")
+    channels_create_v1(user1.get("auth_user_id"), "channel1", True)
+
+    with pytest.raises(InputError):
+        channel_join_v1(user2.get("auth_user_id"), "")
+
+# invalid user id 
+def test_joining_invalid_user():
+    clear_v1()
+    user1 = auth_register_v1("visejoy@gmail.com", "sikk43242342", "Jonathan", "Qiu")
+    new = channels_create_v1(user1.get("auth_user_id"), "channel1", True)
+    temp = 21492144
+    
+    with pytest.raises(InputError):
+        channel_join_v1(temp, new.get("channel_id"))
+
+
+# invalid channel id
+def test_joining_invalid_channel():
+    clear_v1()
+    user1 = auth_register_v1("visejoy@gmail.com", "sikk77466", "Jonathan", "Qiu")
+    user2 = auth_register_v1("thomaschen@gmail.com", "rak54354543", "Thomas", "Chen")
+    channels_create_v1(user1.get("auth_user_id"), "channel1", True)
+
+    temp = 312312321321
+
+    with pytest.raises(InputError):
+        channel_join_v1(user2.get("auth_user_id"), temp)
+
+    
+# private channel accesserror...?
+
+def test_private_channel():
+    clear_v1()
+    user1 = auth_register_v1("visejoy@gmail.com", "sikk432432", "Jonathan", "Qiu")
+    user2 = auth_register_v1("thomaschen@gmail.com", "rak42342432", "Thomas", "Chen")
+    new = channels_create_v1(user1.get("auth_user_id"), "channel1", False)
+
+    with pytest.raises(AccessError):
+        channel_join_v1(user2["auth_user_id"], new["channel_id"])
+
 
