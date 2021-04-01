3d615fb4c155489f5a337a3741439277bbd64cae
Rodney <z5338429@unsw.edu.au>
2021-04-02 00:00:46 +1100

    Updated database to hold secret

--- src/database.py
+++ src/database.py
@@ -1,2 +1,14 @@
+'''
+userData = {
+    "name_first": name_first,
+    "name_last": name_last,
+    "email": email,
+    "password": password,
+    "id": userID,
+    "handle": userHandle, 
+}
+accData is list of userData of above format
+'''
 accData = []
 channelList = []
+secretSauce = "placeholder"
