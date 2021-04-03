a6bcfd59e4956043263070368152757cb95764e7
Colin Ngo <z5310745@ad.unsw.edu.au>
2021-04-03 17:32:59 +1100

    fixed some helper functions

--- src/helper.py
+++ src/helper.py
@@ -1,16 +1,18 @@
-from src.database import accData, channelList, secretSauce
+from src.database import data, secretSauce
+from src.error import InputError, AccessError
+import jwt
 
 
 def valid_userid(auth_user_id):
     # Check if user id is valid
-    for user in accData:
+    for user in data["accData"]:
         if user.get("id") is auth_user_id:
             return True
     return False
 
 def valid_channelid(channel_id):
     # Check if channel id is valid
-    for channel in channelList:
+    for channel in data["channelList"]:
         if channel.get("id") is channel_id:
             return True
     return False
@@ -18,7 +20,7 @@ def valid_channelid(channel_id):
 
 def check_channelprivate(channel_id):
 
-    for channel in channelList:
+    for channel in data["channelList"]:
         if channel.get("id") is channel_id:
             if channel.get("is_public") is True:
                 return False
@@ -26,7 +28,7 @@ def check_channelprivate(channel_id):
 
 def check_useralreadyinchannel(auth_user_id, channel_id):
 
-    for channel in channelList:
+    for channel in data["channelList"]:
         if channel.get("id") is channel_id:
             for member in channel["member_ids"]:
                 if auth_user_id is member:
@@ -35,7 +37,7 @@ def check_useralreadyinchannel(auth_user_id, channel_id):
 
 def check_messageid(message_id):
 
-    for i in channelList:
+    for i in data["channelList"]:
         for message1 in i['messages']:
             if message1.get('message_id') is message_id:
                 return False
@@ -43,7 +45,7 @@ def check_messageid(message_id):
 
 def getchannelID(message_id):
 
-    for i in channelList:
+    for i in data["channelList"]:
         for message1 in i['messages']:
             if message1.get('message_id') is message_id:
                 channel_id1 = i.get("channel_id")
@@ -52,60 +54,26 @@ def getchannelID(message_id):
 
 def checkOwner(auth_user_id, channel_id):
 
-    for channel in channelList:
-        if channel.get["channel_id"] is channel_id:
-            for users in channel.get("owner_ids")
+    for channel in data["channelList"]:
+        if channel["id"] is channel_id:
+            for users in channel["owner_ids"]:
                 if users is auth_user_id:
                     return True
 
     return False
 
-def search_email(email):
-    for items in accData:
-        if items["email"] == email:
-            return True
-    return False
-
-def verify_password(email, password):
-    for items in accData:
-        if items["email"] == email:
-            if items["password"] == password:
-                return True
-            else:
-                return False
-
-def search_handle(currUserHandle):
-    for items in accData:
-        if items["handle"] == currUserHandle:
-            return True
-    return False
-
-def get_user_id(email):
-    for items in accData:
-        if items["email"] == email:
-            userID = items["id"]
-            return userID
-
-def append_handle(currUserHandle):
-    # Check number of users with same handle
-    availableNumber = 0
-    while True:
-        testModCurrUserHandle = currUserHandle + str(availableNumber)
-        if search_handle(testModCurrUserHandle):
-            # Handle isnt avaiable, increment availableNumber
-            availableNumber += 1
-        else:
-            # Handle is available, return number
-            return testModCurrUserHandle
-
-def create_handle(first, last):
-    createUserHandle = first + last
-    createUserHandle = createUserHandle.lower()
-    createUserHandle = createUserHandle.replace("@", "")
-    return createUserHandle
 
 
 def detoken(token):
     u_id = jwt.decode(token, secretSauce, algorithm="HS256")
 
     return u_id["auth_user_id"]
+
+def get_user_id_from_token(token):
+    sessionId = jwt.decode(token, secretSauce, algorithms="HS256")
+    for user in data["accData"]:
+        for session in user["sessions"]:
+            if sessionId["sessionId"] == session:
+                return user["id"]
+    
+    raise InputError("Token does not exist")
