4154e41a1640c6f64ada5eca11e6b023831426e9
Colin Ngo <z5310745@ad.unsw.edu.au>
2021-04-03 19:59:25 +1100

    fixed channel functions

--- src/channels.py
+++ src/channels.py
@@ -1,12 +1,25 @@
 import re
-from src.database import accData, channelList
-from src.auth import auth_register_v1, auth_login_v1
+from src.database import data
+from src.auth import auth_register_v2, auth_login_v2, auth_logout_v1
 from src.error import InputError, AccessError
+from src.helper import get_user_id_from_token
 
-
-def channels_list_v1(auth_user_id):
+'''
+channels_list_v1 takes in a user_id string.
+The functions then checks if the user_id is valid.
+If the user_id is valid, the function then returns all the channels associated
+with the user_id in a list.
+Arguments:
+    auth_user_id (string) - ID of user
+Exceptions:
+    AccessError - Occurs when given id does not match accData
+Return Value:
+    Returns list | {'channels': newchannelList}
+'''
+def channels_list_v1(token):
+    auth_user_id = get_user_id_from_token(token)
     id_status = False
-    for user in accData:
+    for user in data["accData"]:
         if user.get("id") is auth_user_id:
             id_status = True
             break
@@ -15,20 +28,30 @@ def channels_list_v1(auth_user_id):
         raise AccessError("Error: Invalid user id")
 
     newchannelList = []
-    for channel in channelList:
+    for channel in data["channelList"]:
         if auth_user_id in channel.get('member_ids'):
                 channelDict = {}
                 channelDict['channel_id'] = channel.get('id')
                 channelDict['name'] = channel.get('name')
                 newchannelList.append(channelDict)
-    
 
     return {'channels': newchannelList}
 
-
-def channels_listall_v1(auth_user_id):
+'''
+channels_list_v1 takes in a user_id string.
+The functions then checks if the user_id is valid.
+If the user_id is valid, the function then returns all channels.
+Arguments:
+    auth_user_id (string) - ID of user
+Exceptions:
+    AccessError - Occurs when given id does not match accData
+Return Value:
+    Returns list | {'channels': newchannelList}
+'''
+def channels_listall_v1(token):
+    auth_user_id = get_user_id_from_token(token)
     id_status = False
-    for user in accData:
+    for user in data["accData"]:
         if user.get("id") is auth_user_id:
             id_status = True
             break
@@ -37,7 +60,7 @@ def channels_listall_v1(auth_user_id):
         raise AccessError("Error: Invalid user id")
 
     newchannelList = []
-    for channel in channelList:
+    for channel in data["channelList"]:
         channelDict = {}
         channelDict['channel_id'] = channel.get('id')
         channelDict['name'] = channel.get('name')
@@ -45,7 +68,30 @@ def channels_listall_v1(auth_user_id):
 
     return {'channels': newchannelList}
 
-def channels_create_v1(auth_user_id, name, is_public):
+'''
+channels_create_v1 takes in a user id, a specified channel name, and a boolean for 
+whether or not the channel is intended to be public. This function creates an empty channel dictionary 
+and appends the user id as an owner and member of the channel, then returns the newly created
+channel id.
+
+Arguments:
+    auth_user_id (integer) - User id created by auth_register_v1
+    name (string) - Name of the channel
+    is_public (boolean) - Either true or false, sets the channel to public or private
+
+Exceptions:
+    InputError - Occurs when length of name is greater than 20 characters
+    InputError - Occurs when length of name is less than 1 character (not listed on spec but added anyways)
+    AccessError - Occurs when auth_user_id is not valid
+
+Return Value:
+    Returns channel_id | 'channel_id': channel_id,
+
+'''
+
+def channels_create_v1(token, name, is_public):
+    auth_user_id = get_user_id_from_token(token)
+    
     '''
     Creates a new channel with that name that is either a public or private channel
     '''
@@ -58,7 +104,7 @@ def channels_create_v1(auth_user_id, name, is_public):
 
     # Check if valid user id
     id_status = False
-    for user in accData:
+    for user in data["accData"]:
         if user.get("id") is auth_user_id:
             id_status = True
             break
@@ -66,7 +112,7 @@ def channels_create_v1(auth_user_id, name, is_public):
     if id_status is False:
         raise AccessError("Error: Invalid user id")
     
-    channel_id = len(channelList)
+    channel_id = len(data["channelList"])
 
     channelData = {
         'name': name,
@@ -80,13 +126,12 @@ def channels_create_v1(auth_user_id, name, is_public):
     # Adding user data
     channelData['owner_ids'].append(auth_user_id)
     channelData['member_ids'].append(auth_user_id)
-
-    channelList.append(channelData)
-
+    data["channelList"].append(channelData)
 
     return {
         'channel_id': channel_id,
     }
+
 
 
 '''
