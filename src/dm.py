91f9a1beba4cf6029dcf502afa2bb5c40150a51b
Jonathan <z5310747@ad.unsw.edu.au>
2021-04-04 18:41:35 +1000

    Completed DM code and Fixed some tests as well

--- src/dm.py
+++ src/dm.py
@@ -1,7 +1,7 @@
 import re
 from src.database import data
 from src.error import InputError, AccessError
-from src.utils import get_user_id_from_token, make_dm_name, valid_dmid
+from src.utils import get_user_id_from_token, make_dm_name, valid_dmid, valid_userid
 
 '''
 Direct Messages
@@ -58,7 +58,62 @@ def dm_list_v1(token):
 def dm_invite_v1(token, dm_id, u_id):
 '''
 
-#def dm_messages_v1(token, dm_id, start):
+def dm_messages_v1(token, dm_id, start):
+
+    auth_user_id = get_user_id_from_token(token)
+
+    # Check if user id is valid
+    if valid_userid(auth_user_id) is False:
+        raise AccessError("Error: Invalid user id")
+
+    # Check if dm id is valid
+    if valid_dmid(dm_id) is False:
+        raise InputError("Error: Invalid dm")
+
+    #Check if user is authorised to be in the dm
+    authorisation = False
+    for dm in data["dmList"]:
+        if dm["id"] is dm_id:
+            for user in dm["member_ids"]:
+                if user is auth_user_id:
+                    authorisation = True
+                    break
+    if authorisation is False:
+        raise AccessError("User is not in dm")
+
+
+
+    # Return Function
+    for dm in data["dmList"]:
+        if dm["id"] is dm_id:
+            messages = dm["messages"]
+
+    if start > len(messages):
+        raise InputError("Start is greater than total number of messages")
+
+    # 0th index is the most recent message... therefore must reverse list?
+    messages.reverse()
+    
+    # start + 50 messages is what is shown, so must create a list with these
+    # messages within and transfer data from messages to messages_shown
+    messages_shown = []
+    end = start + 50
+    msg_amt = 0
+    while msg_amt < 50:
+        # Where we start and increment from
+        starting_index = start + msg_amt
+        if starting_index >= end or starting_index >= len(messages):
+            break
+
+        messages_shown.append(messages[starting_index])
+        msg_amt = msg_amt + 1
+    if len(messages) == 0 or msg_amt < 50:
+        end = -1
+    return {
+        'messages': messages_shown,
+        'start': start,
+        'end': end,
+    }
 
 def dm_leave_v1(token, dm_id):
 
@@ -73,6 +128,9 @@ def dm_leave_v1(token, dm_id):
             for users in dm["member_ids"]:
                 if auth_user_id is users:
                     dm["member_ids"].remove(auth_user_id)
+                    return
+    raise AccessError
+                    
 
 def dm_remove_v1(token, dm_id):
 
@@ -89,3 +147,4 @@ def dm_remove_v1(token, dm_id):
                 raise AccessError
             else:
                 data["dmList"].remove(dm)
+                return
