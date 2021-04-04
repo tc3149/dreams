1744b85487dbc6e6e83f2dab3d724b73fb35e57b
Thomas Chen <z5313390@ad.unsw.edu.au>
2021-04-04 18:27:50 +1000

    Finished implementation for dm list and invite

--- src/dm.py
+++ src/dm.py
@@ -1,20 +1,11 @@
 import re
+from src.error import InputError, AccessError
 from src.database import data
-from src.utils import get_user_id_from_token
+from src.utils import get_user_id_from_token, make_dm_name, valid_userid
 
 '''
 Direct Messages
 '''
-
-def makedmName(u_ids):
-
-    handle_list = []
-    for user in data["accData"]:
-        if user["id"] in u_ids:
-            handle_list.append(user["handle"])
-    sortedHandle = sorted(handle_list)
-    return ",".join(sortedHandle)
-   
 
 def dm_create_v1(token, u_ids):
     
@@ -28,13 +19,13 @@ def dm_create_v1(token, u_ids):
     dm_id = len(data["dmList"])
 
     # Make dm name as alphabetically sorted list of handles
-    dm_name = makedmName(total_ids)
+    dm_name = make_dm_name(total_ids)
 
     dmData = {
         'dm_name': dm_name,
         'id': dm_id,
         'messages': [],
-        'member_ids': [total_ids],
+        'member_ids': total_ids,
         'owner_ids': [auth_user_id],
     }
 
@@ -47,12 +38,62 @@ def dm_create_v1(token, u_ids):
     }
 
 
-'''
+
 def dm_list_v1(token):
+    auth_user_id = get_user_id_from_token(token)
 
+    newdmList = []
 
+    for dm in data["dmList"]:
+        if auth_user_id in dm.get('member_ids'):
+            dmDict = {}
+            dmDict['dm_id'] = dm.get('id')
+            dmDict['dm_name'] = dm.get('dm_name')
+            newdmList.append(dmDict)
+    
+    return {'dms': newdmList}
 
 
 def dm_invite_v1(token, dm_id, u_id):
-'''
+    # Obtain user id from token
+    #
+    auth_user_id = get_user_id_from_token(token)
+
+    # Check if dm_id exists
+    dmExists = False
+    for dm in data["dmList"]:
+        if dm_id is dm["id"]:
+            # Dm exists
+            dmExists = True
+            break
+    if dmExists is False:
+        # Dm doesnt exist
+        raise InputError("DM does not exist")
+
+    # Check if u_id refers to a valid user
+    if valid_userid(u_id) is False:
+        raise InputError("User ID does not exist")
+
+    # Check if the inviter is part of the dm
+    authorised = False
+    for dm in data["dmList"]:
+        if dm.get("id") is dm_id:
+            if auth_user_id in dm.get("member_ids"):
+                authorised = True
+                break
+    if authorised is False:
+        raise AccessError("User is not a part of the DM to be able to invite")
+
+    # Security measures complete
+    # assumption they will not be in the dm already?
+    # Add user id into the list of member ids
+    for dm in data["dmList"]:
+        if dm.get("id") is dm_id:
+            dm["member_ids"].append(u_id)
+
+    return {}
+    
+
+
+
 
