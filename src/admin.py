053a63b13800d0dbd66e8a146e2e0df5085a0a9e
Mansirat Singh Wander <z5257086@ad.unsw.edu.au>
2021-04-05 22:08:58 +1000

    bug fixes

--- src/admin.py
+++ src/admin.py
@@ -1,7 +1,7 @@
 import re
 from src.error import InputError, AccessError
 from src.database import data
-from src.utils import get_user_id_from_token, make_dm_name, valid_userid, valid_dmid,
+from src.utils import get_user_id_from_token, make_dm_name, valid_userid, valid_dmid
 
 def admin_user_remove_v1(token,u_id):
     #get auth_user_id 
@@ -10,37 +10,46 @@ def admin_user_remove_v1(token,u_id):
     #check if auth_user_id is member 
     auth_id_status = valid_userid(auth_user_id)
     #if not a member
-    if auth_id_satus == False:
+    if auth_id_status == False:
         # auth_user_id does not exist
         raise InputError ("User does not exist")
 
     #check if u_id is exists 
-    user_id_satus = valid_userid(u_id)
+    user_id_status = valid_userid(u_id)
     #user does not exist
-    if user_id_satus == False:
+    if user_id_status == False:
         # user_id does not exist
         raise InputError ("User does not exist")
     
     #if user is only owner and is trying to remove themselves
     if auth_user_id == u_id:
         ownerCount = 1
-        for mem in data['accData']['permission_id']
-            if permission_id == 1
-                ownerCount ++ 
+        for mem in data['accData']:
+            if mem['id']['permission'] == 1:
+                ownerCount = ownerCount + 1 
+                break
         
         if ownerCount == 1:
             raise InputError ('User is currently the only owner of Dreams')
     
     #auth_user_id not an owner
-    permission_id = data['accData']['permission_id']
-    if permission_id not 1:
+    permission_id = data['accData'][auth_user_id]['permission']
+    if permission_id != 1:
         raise AccessError("User is not an owner")
+    
+    for channel in data['channelList']:
+        for messages in channel['messages']:
+            if messages['u_id'] is u_id:
+                messages['message'] = "Removed User"
+    for user in data['userProfiles']:
+        if user['u_id'] is u_id:
+            user['name_first'] = 'Removed'
+            user['name_last'] = 'User'
+    remove_data = []
 
-    #remove u_id from database 
-    for user in data['message_ids']['']
-    data['accData']['removed user'] = data['accData']['u_id']
-    del data['accData']['u_id']
-
+    for user in data['accData']:
+        if user['id'] is u_id:
+            data['accData'].remove(user)
 
     return{}
 
@@ -52,36 +61,36 @@ def admin_userpermission_change_v1(token,u_id,permission_id):
     #check if auth_user_id is member 
     auth_id_status = valid_userid(auth_user_id)
     #if not a member
-    if auth_id_satus == False:
+    if auth_id_status == False:
         # auth_user_id does not exist
         raise InputError ("User does not exist")
 
     #check if u_id is exists 
-    user_id_satus = valid_userid(u_id)
+    user_id_status = valid_userid(u_id)
     #user does not exist
-    if user_id_satus == False:
+    if user_id_status == False:
         # user_id does not exist
         raise InputError ("User does not exist")
     
-    #if user is only owner and is trying to remove themselves
+    #if user is only owner and is trying to change their permission while being the only owner
     if auth_user_id == u_id:
         ownerCount = 1
-        for mem in data['accData']['permission_id']
-            if permission_id == 1
-                ownerCount +=
+        for mem in data['accData']['permission']:
+            if permission_id == 1:
+                ownerCount = ownerCount + 1
         
         if ownerCount == 1:
             raise InputError ('User is currently the only owner of Dreams')
     
     #auth_user_id not an owner
-    permission_id = data['accData']['permission_id']
-    if permission_id not 1:
-        raise AccessError("User is not an owner")
+    auth_permission_id = data['accData'][auth_user_id]['permission']
+    if auth_permission_id != 1:
+        raise AccessError('User is not an owner')
 
     #permission_id is not valid
     if permission_id is 1 or permission_id is 2:
         #replace value of key permission_id
-        data['accData'][u_id]['permission_id'] = permission_id
+        data['accData'][u_id]['permission'] = permission_id
 
     else:
         raise InputError('permission_id does not refer to a value permission')
